0b3aa928233a9930b48f19a832c17d38dee9c96c
Gaby Ecanow <marcifirfer@loulies-macbook-pro.local>
2016-07-26 09:58:16 -0500

    Committing more stuff

--- CollegeProfile/ViewController.swift
+++ CollegeProfile/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    var colleges = ["Northwestern", "Wash U", "UIC"]
+    var colleges = ["Northwestern", "Harvard", "MIT"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
