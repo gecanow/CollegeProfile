08c60a067b664a1105e2c78a24c86ffd76d36112
Gaby Ecanow <marcifirfer@loulies-macbook-pro.local>
2016-07-26 11:39:59 -0500

    Readied everything

--- CollegeProfile/ViewController.swift
+++ CollegeProfile/ViewController.swift
@@ -11,20 +11,83 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    var colleges = ["Northwestern", "Harvard", "MIT"]
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    var colleges : [College] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        editButton.tag = 0
+        
+        colleges.append(College(name: "Northwestern", location: "Evanston", enrollment: 100, image: UIImage(named: "Northwestern")!))
+        
+        colleges.append(College(name: "Harvard", location: "Unknown", enrollment: 100, image: UIImage(named: "Harvard")!))
+        
+        colleges.append(College(name: "MIT", location: "Massachussetts", enrollment: 100, image: UIImage(named: "MIT")!))
     }
     
+    // Set up the table view
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
-        cell.textLabel?.text = colleges[indexPath.row]
+        cell.textLabel?.text = colleges[indexPath.row].name
         return cell
+    }
+    
+    
+    // Allow entries to be deleted
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            colleges.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    // Allow entries to be added
+    @IBAction func onTappedAdd(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Add College Here"
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    // Allow entries to be re-ordered
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let college = colleges[sourceIndexPath.row]
+        colleges.removeAtIndex(sourceIndexPath.row)
+        colleges.insert(college, atIndex: destinationIndexPath.row)
+    }
+
+    @IBAction func onTappedEdit(sender: UIBarButtonItem) {
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
     }
     
 }
